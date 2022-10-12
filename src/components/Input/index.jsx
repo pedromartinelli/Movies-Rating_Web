11d12a1b2d8213ed60313021b0843bdcff8ea782
pedro <p.martinellinunes@gmail.com>
2022-10-12 17:58:39 -0300

    Tentando colocar o ...register em um componente.

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,10 @@
 import { Container } from "./styles";
 
-export function Input({ icon: Icon, ...rest }) {
+export function Input({ icon: Icon, registers: register, ...rest }) {
   return (
     <Container>
       {Icon && <Icon size={20} />}
-      <input {...rest} />
+      <input {...register}  {...rest}/>
     </Container>
   );
 };
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,3 +1,7 @@
+import * as zod from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import { Container, Form } from "./styles";
 
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
@@ -9,20 +13,46 @@ import { NavLink } from "../../components/NavLink";
 import backgroundImg from '../../assets/backgroundImg.png';
 
 export function SignUp() {
+
+  const required = { required_error: 'Este campo é obrigatório' };
+
+  const schema = zod.object({
+    name: zod.string(required).min(1),
+    email: zod.string(required).email({ message: 'Endereço de E-mail inválido' }),
+    password: zod.string(required).min(8, { message: 'Mínimo 8 caracteres' })
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: zodResolver(schema)
+  });
+
+  function handleForm(value) {
+    console.log(value)
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit((d) => handleForm(d))}>
         <h1>moviesRating</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
 
         <h2>Crie sua conta</h2>
 
-        <Input
-          icon={FiUser}
-          placeholder='Nome'
-          type='text'
-        />
-        
+        <div>
+          <Input
+            icon={FiUser}
+            placeholder='Nome'
+            type='text'
+            registers={...register('name')}
+          />
+          {errors.name?.message && <p>Error: {errors.name?.message}</p>}
+        </div>
+
+
         <Input
           icon={FiMail}
           placeholder='E-mail'
